ce8d4673b446a62941651ec809ca2097a5042104
manoharmunnur <manoharmunnur3072@gmail.com>
2025-12-19 02:11:21 +0530

    Replace JavaMailSender with Brevo API for OTP

--- src/main/java/manohar/munnur/sqp/service/impl/EmailServiceLocal.java
+++ src/main/java/manohar/munnur/sqp/service/impl/EmailServiceLocal.java
@@ -2,8 +2,8 @@ package manohar.munnur.sqp.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
-//import org.springframework.mail.SimpleMailMessage;
-//import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 import manohar.munnur.sqp.service.EmailService;
@@ -12,15 +12,15 @@ import manohar.munnur.sqp.service.EmailService;
 @Profile("local")
 public class EmailServiceLocal implements EmailService {
 
-//    @Autowired
-//    private JavaMailSender mailSender;
+    @Autowired
+    private JavaMailSender mailSender;
     
     @Override
     public void sendOtpEmail(String to, String otp) {
-//        SimpleMailMessage message = new SimpleMailMessage();
-//        message.setTo(to);
-//        message.setSubject("Your OTP Code");
-//        message.setText("Your OTP is: " + otp + "\nValid for 5 minutes");
-//        mailSender.send(message);
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject("Your OTP Code");
+        message.setText("Your OTP is: " + otp + "\nValid for 5 minutes");
+        mailSender.send(message);
     }
 }
